3cba2350464e7d89566253bf2706d9ebbbd65af0
ivanprovci <ivanprovci@yahoo.com>
2025-04-21 23:54:02 -0700

    change format tabs width

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -2,15 +2,15 @@ import "./particles.js"
 
 // load particles background for Contact section
 // window.particlesJS.load = function(tag_id, path_config_json, callback)
-particlesJS.load('contact', 'assets/particlesjs-config.json');
+particlesJS.load("contact", "assets/particlesjs-config.json")
 
 // copy email to clipboard functionality
-const btnCopyEmail = document.querySelector('#btn-copy-email')
-const tooltipText = document.querySelector('#btn-copy-email span')
-btnCopyEmail.addEventListener( 'click', () => {
-    navigator.clipboard.writeText('ivanprovci@yahoo.com')
-    tooltipText.textContent = "Copied!"
+const btnCopyEmail = document.querySelector("#btn-copy-email")
+const tooltipText = document.querySelector("#btn-copy-email span")
+btnCopyEmail.addEventListener("click", () => {
+	navigator.clipboard.writeText("ivanprovci@yahoo.com")
+	tooltipText.textContent = "Copied!"
 })
-btnCopyEmail.addEventListener( 'mouseleave', () => {
-    tooltipText.textContent = "Copy"
+btnCopyEmail.addEventListener("mouseleave", () => {
+	tooltipText.textContent = "Copy"
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./docs/*.{html,js}"],
-    theme: {
-      extend: {
-        colors: {
-            primary: '#0d0c11',
-            gold: '#a37b39',
-        }
-      },
-      fontFamily: {
-        inter: 'Inter, sans-serif'
-      },
-      fontWeight: {
-        regular: 400,
-        medium: 500,
-        bold: 700,
-        extrabold: 800,
-        black: 900
-      }
-    },
-    plugins: [],
+	content: ["./docs/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#0d0c11",
+				gold: "#a37b39",
+			},
+		},
+		fontFamily: {
+			inter: "Inter, sans-serif",
+		},
+		fontWeight: {
+			regular: 400,
+			medium: 500,
+			bold: 700,
+			extrabold: 800,
+			black: 900,
+		},
+	},
+	plugins: [],
 }
